cb305614c288e08cd82864806e547cee8fb9fba0
Ananda Ridart <114266023+anandaridart@users.noreply.github.com>
2023-03-15 21:04:38 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(_name_)
+
+menu = """
+<a href="/">Página Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<br>
+"""
+
+@app.route("/")
+def index():
+  return menu + ""Olá, eu sou Ananda! Bem vinda ao meu site teste!"
+
+app.route("/sobre")
+def sobre():
+  return menu +  "Este é meu site para a aula de Algoritmos de Automação"
+
+app.route("/contato")
+def contato():
+  return menu + "meu contato é anandarduarte@gmail.com"
+  
+ 
